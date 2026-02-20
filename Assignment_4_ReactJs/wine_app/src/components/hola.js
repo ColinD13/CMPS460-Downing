e3e1cf68c443ee4324b4ef7207d039500765d0bd
ColinD13 <downingcolin13@gmail.com>
2026-02-20 10:35:40 -0500

    Adding notes
    Added a note for me to look back on laterz

--- Assignment_4_ReactJs/wine_app/src/components/hola.js
+++ Assignment_4_ReactJs/wine_app/src/components/hola.js
@@ -4,6 +4,7 @@ import React from 'react';
 //     return <h1>Hello world!</h1>
 // }
 
+//props is what gets sent to the const when we call Hola in App.js, props.Name lets us retrieve the Name that we sent to the Hola const
 const Hola = props => {
     console.log(props);
     return <h1>Hello {props.name} aka {props.superhero}</h1>;
